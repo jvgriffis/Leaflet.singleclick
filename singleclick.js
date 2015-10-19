41b423c90d27889ee151396925fcef2dbacaeeb0
Iván Sánchez Ortega <ivan@mazemap.no>
2015-10-19 11:39:05 +0200

    Beautify code: unique-r method names, joint event declaration, line width

--- singleclick.js
+++ singleclick.js
@@ -1,22 +1,25 @@
 L.Evented.addInitHook( function () {
-    this._timerId = null;
+    this._singleClickTimeout = null;
     this.on( 'click', this._scheduleSingleClick, this );
-    this.on( 'dblclick', this._cancelSingleClick, this );
-    this.on( 'dragstart', this._cancelSingleClick, this );
-    this.on( 'zoomstart', this._cancelSingleClick, this );
+    this.on( 'dblclick dragstart zoomstart', this._cancelSingleClick, this );
 });
 
 L.Evented.include({
 
-	_cancelSingleClick : function(){
-		//This timeout is key to workaround an issue where double-click events are fired in this order on touch browsers: ['click', 'dblclick', 'click'] instead of ['click', 'click', 'dblclick']
-		setTimeout( this._clearTimeout.bind(this), 0 );
-	},
+    _cancelSingleClick : function(){
+        // This timeout is key to workaround an issue where double-click events
+        // are fired in this order on some touch browsers: ['click', 'dblclick', 'click']
+        // instead of ['click', 'click', 'dblclick']
+        setTimeout( this._clearSingleClickTimeout.bind(this), 0 );
+    },
 
     _scheduleSingleClick: function(e) {
-        this._clearTimeout();
+        this._clearSingleClickTimeout();
 
-        this._timerId = setTimeout( this._fireSingleClick.bind(this, e), (this.options.singleClickTimeout || 500) );
+        this._singleClickTimeout = setTimeout(
+            this._fireSingleClick.bind(this, e),
+            (this.options.singleClickTimeout || 500)
+        );
     },
 
     _fireSingleClick: function(e){
@@ -25,13 +28,12 @@ L.Evented.include({
         }
     },
 
-    _clearTimeout: function(){
-        if (this._timerId != null)
-        {
-            clearTimeout( this._timerId );
-            this._timerId = null;
+    _clearSingleClickTimeout: function(){
+        if (this._singleClickTimeout != null) {
+            clearTimeout( this._singleClickTimeout );
+            this._singleClickTimeout = null;
         }
     }
 
-})
+});
 
